579767b1bf3f7c14dd1f0261ee1cfe61f3745c91
merlebleue <ars.julien@gmail.com>
2023-12-21 23:22:42 +0100

    work

--- template_plotly.py
+++ template_plotly.py
@@ -3,7 +3,8 @@ import plotly.io as pio
 
 pio.templates["patterndetectives"] = go.layout.Template(
     layout=dict(
-    colorscale_sequential = ["#6d45ae", "#9540b2", "#ba36b1", "#de24ab", "#ff0085"]
+    colorscale_sequential = ["#6d45ae", "#9540b2", "#ba36b1", "#de24ab", "#ff0085"],
+    colorscale_diverging = ["#de24ab",  "#968478", "#6d45ae"],
     colorway = ["#ff0085", "#de24ab", "#ba36b1", "#9540b2", "#6d45ae", "#705589", "#392323", "#968478"],
     paper_bgcolor = "#f9f4e3",
     plot_bgcolor = "#e9dcc8"
