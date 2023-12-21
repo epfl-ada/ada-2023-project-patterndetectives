bd8be848bb8a1a84e6adc9dd1d328143423dc3d5
merlebleue <ars.julien@gmail.com>
2023-12-21 23:32:51 +0100

    Update plotly

--- template_plotly.py
+++ template_plotly.py
@@ -9,6 +9,7 @@ pio.templates["patterndetectives"] = go.layout.Template(
     paper_bgcolor = "#f9f4e3",
     plot_bgcolor = "#e9dcc8",
     font_color="#392323",
+    font_family="Raleway"
     )
 )
 
